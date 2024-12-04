9e118d76e9e3afc727e77152b40692a0d750185c
Dune <dnn47@drexel.edu>
2024-12-03 22:14:27 -0500

    Add pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+function UserPage() {
+  return <h1>User Page</h1>;
+}
+
+function AnimalsPage() {
+  return <h1>Animals Page</h1>;
+}
+
+function ApplicationPage() {
+  return <h1>Application Page</h1>;
+}
+
+function HomePage() {
+  return <h1>Welcome to the Home Page</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/user" element={<UserPage />} />
+        <Route path="/animals" element={<AnimalsPage />} />
+        <Route path="/application" element={<ApplicationPage />} />
+      </Routes>
     </div>
   );
 }
